4ce8cc590a6c18e4f7574e9f018e65a28c7197c7
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-08-14 18:25:32 +0530

    added operate function to handle different math operations

--- main.js
+++ main.js
@@ -21,3 +21,22 @@ function modulo(num1, num2) {
     return num1 % num2;
 }
 
+function operate(number1, number2, operator) {
+    const operations = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div,
+        '%': modulo
+    };
+
+    if (
+        typeof number1 === "number" &&
+        typeof number2 === "number" &&
+        operations.hasOwnProperty(operator)
+    ) {
+        return operations[operator](number1, number2);
+    }
+
+    return;
+}
