b190dcc1d4d1ec0f4a1da2e791ca010f17d2a65b
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-08-14 17:46:51 +0530

    Added functions returning basic math operations answers

---
+++ main.js
@@ -0,0 +1,23 @@
+let number1, number2, operator;
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function mul(num1, num2) {
+    return num1 * num2;  
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function modulo(num1, num2) {
+    return num1 % num2;
+}
+
